6d9742f4b0ffe0863c2e685a18ccfb9a0ec58f67
gayanimadahapola <gayanimadahapola@users.github.com>
2020-01-29 10:27:14 +0530

    Initial Commit

---
+++ HBO_Now/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HBO_Now
+//
+//  Created by Gayani Madubhashini on 1/29/20.
+//  Copyright © 2020 Gayani Madubhashini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
